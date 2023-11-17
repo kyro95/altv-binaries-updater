c275dfb8d2150845e5cbafe39a184f9dd916b330
kyro <46943169+kyro95@users.noreply.github.com>
2023-11-17 15:39:32 +0100

    Fix install.ps1

--- install.ps1
+++ install.ps1
@@ -1,33 +1,21 @@
 $repoOwner = 'kyro95'
 $repoName = 'altv-binaries-updater'
 $releaseTag = 'latest'
-
 $apiUrl = "https://api.github.com/repos/$repoOwner/$repoName/releases/$releaseTag"
-
 $response = Invoke-RestMethod -Uri $apiUrl
+$downloadUrl = $response.assets[1].browser_download_url
+$extractPath = "C:\Program Files\altv-binaries-updater"
 
-$downloadUrl = $response.assets[0].browser_download_url
-
-$downloadPath = "$env:TEMP\Release.zip"
-$extractPath = "$env:ProgramFiles\altv-binaries-updater"
-
+$downloadPath = "$env:TEMP\AltVUpdater.zip"
 Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
-
-if (-not (Test-Path $extractPath)) {
-    New-Item -ItemType Directory -Path $extractPath | Out-Null
-}
 
 Expand-Archive -Path $downloadPath -DestinationPath $extractPath -Force
 
 Remove-Item $downloadPath -Force
 
-$aliasContent = @"
-function altv-update {
-    Set-Location "$extractPath"
-    Write-Host "You are now in the altv-binaries-updater directory."
-}
-"@
+$newPath = "$extractPath"
+[Environment]::SetEnvironmentVariable("Path", "$($env:Path);$newPath", [System.EnvironmentVariableTarget]::Machine)
 
-Add-Content -Path $PROFILE.AllUsersCurrentHost -Value $aliasContent
+Set-Alias -Name altv-updater -Value "$newPath\AltV.Binaries.Updater.exe" -Scope Global
 
-Write-Host "altv-binaries-updater has been installed, and the global alias 'altv-update' has been added."
+Write-Host "AltV Updater installed successfully. Please restart your terminal to use it."
